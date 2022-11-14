08f608198ef41430a46727a35fe7ac34835e5dce
Joyee Yang <joyeeyiyi@hotmail.com>
2022-11-14 18:07:15 -0500

    added subscriptions

--- PB/TFC/Subscriptions/apps.py
+++ PB/TFC/Subscriptions/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SubscriptionsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Subscriptions'
+# class SubscriptionsConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'subscriptions'
