54e180f5f235d98b071d8913ee5be8b0f2fc7f1e
icehunt <penguin@tom.ac>
2022-11-16 18:53:11 -0500

    Fix for issues with user fields

--- PB/TFC/User/serializers.py
+++ PB/TFC/User/serializers.py
@@ -10,14 +10,14 @@ class UserSerializer(serializers.ModelSerializer):
 
     def save(self):
         user = User(
-            email=self.validated_data['email'],
-            first_name=self.validated_data['first_name'],
-            last_name=self.validated_data['last_name'],
-            phone_number=self.validated_data['phone_number'],
-            avatar=self.validated_data['avatar']
+            email=self.validated_data.get('email'),
+            first_name=self.validated_data.get('first_name'),
+            last_name=self.validated_data.get('last_name'),
+            phone_number=self.validated_data.get('phone_number'),
+            avatar=self.validated_data.get('avatar')
         )
 
-        password = self.validated_data['password']
+        password = self.validated_data.get('password')
         user.set_password(password)
         user.save()
         return user
@@ -30,11 +30,11 @@ class PaymentInfoSerializer(serializers.ModelSerializer):
 
     def save(self, **kwargs):
         payment_info = PaymentInfo(
-            card_number=self.validated_data['card_number'],
-            expiry=self.validated_data['expiry'],
-            cvv=self.validated_data['cvv'],
-            postal_code=self.validated_data['postal_code'],
-            user=self.context['user']
+            card_number=self.validated_data.get('card_number'),
+            expiry=self.validated_data.get('expiry'),
+            cvv=self.validated_data.get('cvv'),
+            postal_code=self.validated_data.get('postal_code'),
+            user=self.context.get('user')
         )
         payment_info.save()
         return payment_info
