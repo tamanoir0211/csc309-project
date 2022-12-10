5b4bcddbd765245ed1867767d7ec7535b35a2358
Penguin <penguin@tom.ac>
2022-12-09 20:27:02 -0500

    Removed PB

--- PB/TFC/subscriptions/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from subscriptions.models import Subscription
-
-
-class SubscriptionSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Subscription
-        fields = ['price', 'length_months', 'sub_id']
